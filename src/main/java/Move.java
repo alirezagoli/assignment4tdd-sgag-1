c4157360acfdcb04a75ea311a8512221a0616cbb
Sara Gholami <srghlm6@gmail.com>
2019-02-24 15:55:20 -0700

    REFACTOR - Move
    Added comment to Move class

--- src/main/java/Move.java
+++ src/main/java/Move.java
@@ -3,6 +3,11 @@ public class Move {
     private final int pileIndex;
     private final int numStone;
 
+    /**
+     *  Move class represent a move by a player during the game
+     * @param pileIndex represents the index of a pile selected by the player
+     * @param numStone represents the number of stone player want to remove from a pile
+     */
     public Move(int pileIndex, int numStone) {
         this.pileIndex = pileIndex;
         this.numStone = numStone;
