6e941eb65756c0ff1c379563c237e08d11d97443
Alireza <alirezagoli.mail@gmail.com>
2019-02-25 00:36:33 -0700

    Add documentation for the play method of ComputerPlayer class.

--- src/main/java/ComputerPlayer.java
+++ src/main/java/ComputerPlayer.java
@@ -2,6 +2,13 @@ import java.util.Random;
 
 public class ComputerPlayer {
 
+    /**
+     * This player plays completely randomly.
+     * It will choose one of the piles and remove a random number of stones from it.
+     *
+     * @param piles the list of piles on the board
+     * @return the tuple indicating the computer move
+     */
     public Move play(int[] piles) {
         Random random = new Random();
         int selectedPile, numStone;
