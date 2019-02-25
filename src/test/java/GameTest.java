2e58deb27458d0f19a66c3753f26de8318073886
Alireza <alirezagoli.mail@gmail.com>
2019-02-25 13:26:52 -0700

    RED - Add test for showMenu method of Game class.

---
+++ src/test/java/GameTest.java
@@ -0,0 +1,35 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.*;
+
+public class GameTest {
+    Game game;
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @Before
+    public void setUp() throws Exception {
+        game = new Game();
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        System.setOut(originalOut);
+    }
+
+    @Test
+    public void showMenu() {
+        game.showMenu();
+        String expectedOutput = "Please choose one of the following options PlayerName\n" +
+                "1. Start new game\n" +
+                "2. How to play?\n" +
+                "3. Exit\n";
+        assertEquals(expectedOutput, outContent.toString());
+    }
+}
