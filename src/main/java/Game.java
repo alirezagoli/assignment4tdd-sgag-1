d4ddaeb3f55fd3c3f5a50bdd3dc92b7889158b11
Alireza <alirezagoli.mail@gmail.com>
2019-02-25 13:31:55 -0700

    GREEN - Pass the test for the showMenu method of Game class.

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,6 +1,9 @@
 public class Game {
 
     void showMenu() {
-
+        System.out.print("Please choose one of the following options PlayerName\n" );
+        System.out.print("1. Start new game\n");
+        System.out.print("2. How to play?\n");
+        System.out.print("3. Exit\n");
     }
 }
