8d375855add7b63d0d063c87a683423f3fad6414
Sara Gholami <srghlm6@gmail.com>
2019-02-24 18:29:49 -0700

    GREEN - Board - print
    print function passes its test

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -20,7 +20,42 @@ public class Board {
     }
 
     public void print() {
+        int maxStone = 0;
+        int[] piles2 = new int[piles.length];
+        for (int i = 0; i < piles.length; i++) {
+            if (piles[i] > maxStone) {
+                maxStone = piles[i];
+            }
+            piles2[i] = piles[i];
+        }
 
+        StringBuilder sb = new StringBuilder();
+        System.out.println();
+        for (int i = maxStone; i > 0; i--) {
+            for (int j = 0; j < piles2.length; j++) {
+                if (piles2[j] == i) {
+                    sb.append("\tX");
+                    piles2[j] = piles2[j] - 1;
+                } else {
+                    sb.append("\t ");
+                }
+            }
+            System.out.println(sb.toString());
+            sb.setLength(0);
+        }
+
+        for (int i = 0; i < piles.length; i++) {
+            sb.append("\u203E\u203E\u203E\u203E\u203E\u203E\u203E");
+        }
+        System.out.println(sb.toString());
+        sb.setLength(0);
+
+        for (int i = 0; i < piles.length; i++) {
+            sb.append("\t");
+            sb.append((i + 1));
+        }
+        System.out.println(sb.toString());
+        System.out.println("\n");
     }
 
     public int[] getPiles() {
