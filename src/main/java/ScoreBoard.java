da8944cd40170e749e477b0544c790b453ca8993
Alireza <alirezagoli.mail@gmail.com>
2019-02-24 17:21:24 -0700

    REFACTOR - Refactor incrementWin method of ScoreBoard class.

--- src/main/java/ScoreBoard.java
+++ src/main/java/ScoreBoard.java
@@ -1,13 +1,20 @@
 public class ScoreBoard {
 
-    public void incrementWin() {
+    private int win;
 
+    public ScoreBoard() {
+        win = 0;
+    }
+
+    public void incrementWin() {
+        win= win+1;
     }
 
     public void setWin(int i) {
+        win=i;
     }
 
     public int getWin() {
-        return 1;
+        return win;
     }
 }
