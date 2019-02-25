a0c627bbbc91f2f02a1ddc3acd7a26db6fdf5570
saraGhlm <srghlm6@gmail.com>
2019-02-25 11:02:40 -0700

    GREEN - Nim - isInteger
    isInteger function is passing its test

--- src/main/java/Nim.java
+++ src/main/java/Nim.java
@@ -8,6 +8,11 @@ public class Nim {
     }
 
     public boolean isInteger(String s) {
-
+        Scanner sc = new Scanner(s.trim());
+        if (!sc.hasNextInt(10)) return false;
+        // we know it starts with a valid int, now make sure
+        // there's nothing left!
+        sc.nextInt(10);
+        return !sc.hasNext();
     }
 }
