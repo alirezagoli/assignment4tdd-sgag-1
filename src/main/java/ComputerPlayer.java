6ff9a285ef11d7002b318052d03ca59017a7cd5a
Alireza <alirezagoli.mail@gmail.com>
2019-02-25 00:31:58 -0700

    REFACTOR - Refactor play method of ComputerPlayer class.

--- src/main/java/ComputerPlayer.java
+++ src/main/java/ComputerPlayer.java
@@ -3,6 +3,15 @@ import java.util.Random;
 public class ComputerPlayer {
 
     public Move play(int[] piles) {
-        return new Move(1, 3);
+        Random random = new Random();
+        int selectedPile, numStone;
+        while (true) {
+            selectedPile = random.nextInt(piles.length);
+            if (piles[selectedPile] == 0)
+                continue;
+            numStone = random.nextInt(piles[selectedPile]) + 1;
+            break;
+        }
+        return new Move(selectedPile, numStone);
     }
 }
